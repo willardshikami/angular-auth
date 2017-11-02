76eaf60075228c5273237e44ed6b6baceeef9a2c
willardshikami <willardshikami@gmail.com>
2017-11-02 22:09:12 +0300

    refactoring

---
+++ app/signin/signin.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.signin module', function() {
+
+  beforeEach(module('myApp.signin'));
+
+  describe('signin controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var signinCtrl = $controller('SigninCtrl');
+      expect(signinCtrl).toBeDefined();
+    }));
+
+  });
+});
