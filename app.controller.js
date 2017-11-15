7f2841421ea11843751e56448a89f5b3ca36ab66
willardshikami <willardshikami@gmail.com>
2017-11-15 14:19:40 +0300

    added smooth scroll + angular modal dependency

--- app.controller.js
+++ app.controller.js
@@ -1,6 +1,5 @@
 angular.module('healthixSite')
     healthixSite.controller('healthixController', ['$scope', '$http', '$location', '$routeParams',function($scope, $http, $location, $routeParams) {
-
-
+        
     }]);
     
