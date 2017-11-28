722f724c01c0145fc71e5f144d6b5709c35c6112
willardshikami <willardshikami@gmail.com>
2017-11-28 21:09:03 +0300

    updated controller

--- app.module.js
+++ app.module.js
@@ -1,39 +1,39 @@
 var app = angular.module('healthixSite', ['ngRoute']);
 
-app.config(function($routeProvider) {
+app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
-        controller: 'healthixController',
-        templateUrl: 'templates/home.template.html'
-    });
-    $routeProvider.when('/eclaim-management', {
-        controller: 'healthixController',
-        templateUrl: 'templates/e-claim.template.html'
-    });
-    $routeProvider.when('/insurecheck', {
-        controller: 'healthixController',
-        templateUrl: 'templates/insurecheck.template.html'
-    });
-    $routeProvider.when('/e-enrollment', {
-        controller: 'healthixController',
-        templateUrl: 'templates/e-enrollment.template.html'
-    });
-    $routeProvider.when('/e-prescription', {
-        controller: 'healthixController',
-        templateUrl: 'templates/e-prescription.template.html'
-    });
-    $routeProvider.when('/e-authletter', {
-        controller: 'healthixController',
-        templateUrl: 'templates/e-authletter.template.html'
-    }); 
-    $routeProvider.when('/biometric', {
-        controller: 'healthixController',
-        templateUrl: 'templates/biometric.template.html'
-    }); 
-    $routeProvider.when('/terminology', {
-        controller: 'healthixController',
-        templateUrl: 'templates/terminology.template.html'
-    })
-    .otherwise({
-        redirectTo: '/'
-    });
-});
+            controller: 'healthixController',
+            templateUrl: 'templates/home.template.html'
+        })
+        .when('/eclaim-management', {
+            controller: 'healthixController',
+            templateUrl: 'templates/e-claim.template.html'
+        })
+        .when('/insurecheck', {
+            controller: 'healthixController',
+            templateUrl: 'templates/insurecheck.template.html'
+        })
+        .when('/e-enrollment', {
+            controller: 'healthixController',
+            templateUrl: 'templates/e-enrollment.template.html'
+        })
+        .when('/e-prescription', {
+            controller: 'healthixController',
+            templateUrl: 'templates/e-prescription.template.html'
+        })
+        .when('/e-authletter', {
+            controller: 'healthixController',
+            templateUrl: 'templates/e-authletter.template.html'
+        })
+        .when('/biometric', {
+            controller: 'healthixController',
+            templateUrl: 'templates/biometric.template.html'
+        })
+        .when('/terminology', {
+            controller: 'healthixController',
+            templateUrl: 'templates/terminology.template.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
