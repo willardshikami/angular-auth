e20b707d7da6fb426559912a6f947c726b7a6a48
willardshikami <willardshikami@gmail.com>
2017-11-28 20:22:20 +0300

    updated

---
+++ app.controller.js
@@ -0,0 +1,6 @@
+var app = angular.module('healthixSite', []);
+
+app.controller('healthixController', ['$scope', '$http', '$location', '$routeParams', 
+function($scope, $http, $location, $routeParams){
+
+}]);
