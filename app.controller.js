e0efcfc20a166ef0a675e001bf1734f3926f0c21
willardshikami <willardshikami@gmail.com>
2017-12-19 00:17:15 +0300

    configured the router with specific templates

--- app.controller.js
+++ app.controller.js
@@ -3,4 +3,20 @@ var app = angular.module('healthixSite', []);
 app.controller('healthixController', ['$scope', '$http', '$location', '$routeParams', 
 function($scope, $http, $location, $routeParams){
 
+  //GET REQUEST
+    var vm = this;
+    $http.get('json/service-info.json').then(function(res){
+        vm.service_info = res.data;
+        console.log(vm.service_info);
+    });
+
+    var receivedData = null;
+    vm.serviceSelect = function(data) {
+        console.log(data);
+        // alert("Would you like to book this service?");
+         var receivedData = data;
+         
+     $state.go('base.booking', {'bookingData': receivedData, 'bookingHos': true});
+    }
+
 }]);
